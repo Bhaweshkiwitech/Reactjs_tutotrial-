f2661a44bd309826584635b10d56895f9abb3594
Bhaweshkiwitech <you@example.com>
2022-08-02 12:15:12 +0530

    render method in reactjs

--- reactjstut01/src/App.js
+++ reactjstut01/src/App.js
@@ -1,19 +1,16 @@
-import PropTypes from "prop-types";
-import React, { Component } from "react";
+import React from "react";
 import User from "./User";
-export default class extends Component {
-  constructor() {
-    super(); //we use super because we use parent class Component (hindi me component ka use karne ke liye ham super() call karte hai)
-    this.state = { data: "bhawesh" };
-    //never call API in constructor
-  }
-  
-  render() {
-    return (
-      <div>
-        <h1>HELLO THERE! {this.state.data}</h1>
-        <User />
-      </div>
-    );
-  }
+import './App.css'
+
+function App() {
+  const[ name,setName]=React.useState("Devesh")
+  return (
+    <div className="App">
+      <h1>Render Method in reactjs</h1>
+      <User name ={name}/>
+      <button onClick={()=>setName("Bhawesh")}>Update name</button>
+    </div>
+  );
 }
+
+export default App;
