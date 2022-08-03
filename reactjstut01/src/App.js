b5ca1c0e3d0add5c54c21e68a3a45f959a8f06d9
Bhaweshkiwitech <you@example.com>
2022-08-03 14:30:19 +0530

    map vs for loop in reactkjs

--- reactjstut01/src/App.js
+++ reactjstut01/src/App.js
@@ -1,12 +1,44 @@
-import React from 'react'
-import {Button} from 'react-bootstrap'
+import React from "react";
+
+
 function App() {
+  // const students = [
+  //   "Bhawesh",
+  //   "Devesh",
+  //   "piyush sir",
+  //   "yuvraj sir",
+  //   "Akhlaq sir",
+  // ];
+
+  const students = [
+    { name: "Bhawesh", contact: 888, email: "bp@gmail.com" },
+    { name: "Devesh", contact: 11, email: "dp22@gmail.com" },
+    { name: "piyush sir", contact: 220, email: "pi232@gmail.com" },
+    { name: "akhlaq sir", contact: 212, email: "akp31232@gmail.com" },
+  ];
+  //map looping
+
+  // students.map((item)=>{
+  //   console.log('my name is ', item);
+  // })
+
+  // for(let i=0;i<=students.length;i++){
+  //   console.log('for loop', students[i]);
+  // }
   return (
-    <div className='App'>
-      <h1>Installing bootstrap</h1>
-      <Button onClick={()=>alert("kya hall hai ")}>Click Me Bro</Button>
+    <div className="App">
+      <h1>Handle Array with List</h1>
+      <table border={12}>
+        {/* use map in reactjs instead of for loop, while loop in reactjs*/}
+        {students.map((data) => (
+          <tr border={12}>
+            <td border={12}>{data.name}</td>
+            <td border={12}>{data.email}</td>
+          </tr>
+        ))}
+      </table>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
