f8b753567f6ce829c9223398fbe0fa300cc613d4
Bhaweshkiwitech <you@example.com>
2022-08-03 12:48:16 +0530

    installing bootstarp using npm install bootstarp react-bootstarp and uses

--- reactjstut01/src/App.js
+++ reactjstut01/src/App.js
@@ -1,16 +1,11 @@
 import React from 'react'
-import './style.css'
-import style from './custom.module.css'
+import {Button} from 'react-bootstrap'
 function App() {
   return (
     <div className='App'>
-    {/* className Styling */}
-      <h1 className='primary'>hello Bhawesh</h1> 
-      {/* inline styling we use double curly brackets first for styling second for object */}
-      <h1 style={{color:'red',backgroundColor:'blue'}}>hello Bhawesh</h1> 
-      {/* we are using modular css here  */}
-      <h1 className={style.success}>hello Bhawesh</h1> 
-      </div>
+      <h1>Installing bootstrap</h1>
+      <Button onClick={()=>alert("kya hall hai ")}>Click Me Bro</Button>
+    </div>
   )
 }
 
