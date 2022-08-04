e21d0c16d94b39223c11ac9f8e23de59ac59cd60
Bhaweshkiwitech <you@example.com>
2022-08-04 11:29:17 +0530

    this is new table with bootstrap

--- reactjstut01/src/App.js
+++ reactjstut01/src/App.js
@@ -2,43 +2,44 @@ import React from "react";
 
 import Table from 'react-bootstrap/Table';
 function App() {
-  // const students = [
-  //   "Bhawesh",
-  //   "Devesh",
-  //   "piyush sir",
-  //   "yuvraj sir",
-  //   "Akhlaq sir",
-  // ];
+
 
   const students = [
-    { name: "Bhawesh", contact: 888, email: "bp@gmail.com" },
-    { name: "Devesh", contact: 11, email: "dp22@gmail.com" },
-    { name: "piyush sir", contact: 220, email: "pi232@gmail.com" },
-    { name: "akhlaq sir", contact: 212, email: "akp31232@gmail.com" },
+    { id:1,name: "Bhawesh", contact: 888, email: "bp@gmail.com" },
+    {  id:2,name: "Devesh", contact: 11, email: "dp22@gmail.com" },
+    { id:3, name: "piyush sir", contact: 220, email: "pi232@gmail.com" },
+    {  id:4,name: "akhlaq sir", contact: 212, email: "akp31232@gmail.com" },
   ];
   
   return (
-    <div className="App">
-      <h1>Handle Array with List</h1>
-      <Table striped bordered hover>
-      <tr>
-          
-          <th> Name</th>
-          <th> Contact</th>
-          <th>Email</th>
-        </tr>
-
-        {/* use map in reactjs instead of for loop, while loop in reactjs*/}
-        {students.map((data) => (
-          <tr>
-            <td>{data.name}</td>
-            <td>{data.contact}</td>
-            <td>{data.email}</td>
-          </tr>
-        ))}
+        <Table striped bordered hover variant="dark">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Contact</th>
+            </tr>
+          </thead>
+          <tbody>
+          {
+            students.map((item)=>
+            <tr>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+              <td>{item.contact}</td>
+            </tr>
+            
+            )
+          }
+           
+      
+          </tbody>
         </Table>
-    </div>
-  );
-}
+   
+   
+   );
+  }
 
 export default App;
