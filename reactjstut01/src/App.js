31ce6f27be0e3d52b51370d73a6232fee4cd13b8
Bhaweshkiwitech <you@example.com>
2022-08-05 15:46:17 +0530

    use of hook useMemo for  reactjs performace in function based component

--- reactjstut01/src/App.js
+++ reactjstut01/src/App.js
@@ -1,15 +1,26 @@
-import React from 'react'
-import User from './User'
+import React, { useState, useMemo } from "react";
+import User from "./User";
 function App() {
-  function parentAlert(data){
-    alert(data.name);
-  }
+  const [count, setCount] = useState(0);
+  const [item, setItem] = useState(10);
+  const multiCountMemo = useMemo(
+    // this help  us not to updated data when item is updated, it is updated when count is updated
+    function multiCount() {
+      return count * 5;     // useMemo  help to improve performance
+    },
+    [count]
+  );
+
   return (
     <>
-    <h1>Lifting State up</h1>
-      <User alert={parentAlert}/>
+      <h1>Lifting State up</h1>
+      <h2>Count:{count}</h2>
+      <h2>item:{item}</h2>
+      <h2> function:{multiCountMemo}</h2>
+      <button onClick={() => setCount(count + 1)}>Update count</button>
+      <button onClick={() => setItem(item * 10)}>Update items</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
