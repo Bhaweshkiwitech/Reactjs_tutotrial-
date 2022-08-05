2b53b6ee9451d6b9b190e43e08c67ad50ac094c2
Bhaweshkiwitech <you@example.com>
2022-08-05 19:45:24 +0530

    using High Order Component in ReactJS

--- reactjstut01/src/App.js
+++ reactjstut01/src/App.js
@@ -1,30 +1,25 @@
+import React,{useState} from "react";
 
-import React, { useRef } from 'react'
 function App() {
-  let inputRef = useRef(null)
-  let inputRef2 = useRef(null)
-
-  function submitForm(e) {
-    e.preventDefault()
-    console.warn("input field 1 value : ", inputRef.current.value)
-    console.warn("input field 2 value : ", inputRef2.current.value)
-    let input3 = document.getElementById('input3').value
-    console.warn("input field 3 value : ", input3)
-
-
-  }
   return (
-    <div className="App">
-      <h1>Uncontrolled Component </h1>
-      <form onSubmit={submitForm} >
-        <input ref={inputRef} type="text" /> <br /> <br />
-        <input ref={inputRef2} type="text" /> <br /> <br />
-        <input id="input3" type="text" /> <br /> <br />
-
-        <button>Submit</button>
-      </form>
-    </div>
+    <>
+      <h1>High Order Component</h1>
+      {/* <Counter/> */}
+      <HOC cmp={Counter}/>
+    </>
   );
+}
+function HOC(props){
+ return <h2 style={{backgroundColor:'red'}}><props.cmp/></h2>
 
 }
+function Counter() {
+  const [count, setCount]=useState(0)
+  return <div>
+      <h3>{count}</h3>
+      <button onClick={()=>setCount(count+1)}>Update</button>
+    </div>
+  
+}
+
 export default App;
