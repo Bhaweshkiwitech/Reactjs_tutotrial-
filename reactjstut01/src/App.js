931889848c83d1dd6fa86526ca702a9e83da055f
Bhaweshkiwitech <you@example.com>
2022-08-05 16:00:53 +0530

    use of ref in reactjs

--- reactjstut01/src/App.js
+++ reactjstut01/src/App.js
@@ -1,26 +1,28 @@
-import React, { useState, useMemo } from "react";
-import User from "./User";
-function App() {
-  const [count, setCount] = useState(0);
-  const [item, setItem] = useState(10);
-  const multiCountMemo = useMemo(
-    // this help  us not to updated data when item is updated, it is updated when count is updated
-    function multiCount() {
-      return count * 5;     // useMemo  help to improve performance
-    },
-    [count]
-  );
+import React, { Component,createRef} from 'react';
 
-  return (
-    <>
-      <h1>Lifting State up</h1>
-      <h2>Count:{count}</h2>
-      <h2>item:{item}</h2>
-      <h2> function:{multiCountMemo}</h2>
-      <button onClick={() => setCount(count + 1)}>Update count</button>
-      <button onClick={() => setItem(item * 10)}>Update items</button>
-    </>
-  );
+class App extends Component {
+  constructor(props){
+    super()
+    this.inputRef=createRef()
+  }
+
+  componentDidMount() {
+    // console.log(this.inputRef.current.value='1000')
+  }
+  getVal(){
+    console.log(this.inputRef.current.value)
+    this.inputRef.current.style.color='red'
+    
+  }
+  render() {
+    return (
+      <>
+        <h1>Ref in Reactjs</h1>
+        <input type="text" ref={this.inputRef}/>
+        <button onClick={()=>this.getVal()}>Click Me </button>
+        </>
+    );
+  }
 }
 
 export default App;
