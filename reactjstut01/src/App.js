f39f99e3892f1ee9b92515af98538759938ee6be
Bhaweshkiwitech <you@example.com>
2022-08-05 19:19:05 +0530

    using uncontrolled component

--- reactjstut01/src/App.js
+++ reactjstut01/src/App.js
@@ -1,15 +1,30 @@
-import React,{useState} from 'react'
 
- function App() {
-  let [val,setVal]=useState('Enter your name Bro')
-  let [item,setItem]=useState('Enter your password Bro')
+import React, { useRef } from 'react'
+function App() {
+  let inputRef = useRef(null)
+  let inputRef2 = useRef(null)
+
+  function submitForm(e) {
+    e.preventDefault()
+    console.warn("input field 1 value : ", inputRef.current.value)
+    console.warn("input field 2 value : ", inputRef2.current.value)
+    let input3 = document.getElementById('input3').value
+    console.warn("input field 3 value : ", input3)
+
+
+  }
   return (
-    <>
-   <h1>Controlled Component</h1>
-   <input type="text" value={val} onChange={(e)=>setVal(e.target.value)}/>
-   <input type="text" value={item} onChange={(e)=>setItem(e.target.value)}/>
-   <h3>Value :{val}</h3>
-    </>
-  )
+    <div className="App">
+      <h1>Uncontrolled Component </h1>
+      <form onSubmit={submitForm} >
+        <input ref={inputRef} type="text" /> <br /> <br />
+        <input ref={inputRef2} type="text" /> <br /> <br />
+        <input id="input3" type="text" /> <br /> <br />
+
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+
 }
 export default App;
